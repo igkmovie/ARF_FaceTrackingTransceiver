7825efb69122b5db5a449ca074223cd70415fb92
井口健一 <igkworks@gmail.com>
2021-08-28 09:21:19 +0900

    OscJack導入・実装

---
+++ Assets/Script/BlendshapeModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class BlendshapeModels
+{
+    public List<BlendshapeModel> shapes = new List<BlendshapeModel>();
+    public HeadPosRot headPosRot = new HeadPosRot();
+}
+[Serializable]
+public class BlendshapeModel 
+{
+    public string Location;
+    public float coefficient;
+}
+[Serializable]
+public class HeadPosRot
+{
+    public Vector3 pos;
+    public Vector4 rot;
+}
---
+++ Assets/Script/OscServer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OscServer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnMessage(float value)
+    {
+        Debug.Log(value);
+    }
+}
